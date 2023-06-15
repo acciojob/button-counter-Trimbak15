32614a9aaaaabe719b3f2b81afd112d6e6a7cafd
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-06-15 11:40:40 +0000

    first-commit

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+const Button = () =>{
+    let [count, setCount] = useState(0);
+    return (
+        <div>
+            <p>Button clicked {count} times</p>
+            <button onClick={() => setCount(count+1)}>Click me</button>
+        </div>
+    )
+}
+export default Button;
